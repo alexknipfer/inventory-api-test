4b42e9f5030c652de2823c9f785bf89aed235aaf
Alex Knipfer <alexanderknipfer@gmail.com>
2018-10-13 21:04:58 -0500

    Add inventory item model

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace InventoryService.Models
+{
+    public class InventoryItem
+    {
+        [BsonId]
+        public ObjectId InternalId { get; set; }
+
+        public string Id { get; set; }
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+    }
+}
