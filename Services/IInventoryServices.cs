9235452eaeefe4c180baaedc185ee0712955729b
Alex Knipfer <alexanderknipfer@gmail.com>
2018-10-13 21:05:12 -0500

    Add inventory service

---
+++ Services/IInventoryServices.cs
@@ -0,0 +1,12 @@
+﻿using InventoryService.Models;
+using System.Collections.Generic;
+
+namespace InventoryService.Services
+{
+    public interface IInventoryServices
+    {
+        InventoryItem AddInventoryItems(InventoryItem items);
+
+        Dictionary<string, InventoryItem> GetInventoryItems();
+    }
+}
---
+++ Services/InventoryServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryService.Models;
+
+namespace InventoryService.Services
+{
+    public class InventoryServices : IInventoryServices
+    {
+        private readonly Dictionary<string, InventoryItem> _inventoryItems;
+
+        public InventoryServices()
+        {
+            _inventoryItems = new Dictionary<string, InventoryItem>();
+        }
+
+        public InventoryItem AddInventoryItems(InventoryItem items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+            return items;
+        }
+
+        public Dictionary<string, InventoryItem> GetInventoryItems()
+        {
+            return _inventoryItems;
+        }
+    }
+}
